6c0ba3d5d78f288c3a37bc7cd8300b6f8a4647a3
EugeneKruglei <eugene787@yandex.ru>
2018-11-28 18:11:06 +0300

    Draw/mobile adaptation unfinished

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -2,63 +2,70 @@ import React from 'react';
 import Link from 'react-router-dom/Link';
 import {NavLink} from 'react-router-dom';
 import './Nav.css';
+import logo from '../assets/MDS_logo.png';
 
 export default function Nav() {
     return (
         <nav className="nav">
-            <NavLink
-                exact to={{
-                pathname: '/home',
-                state: {prev: false},
-            }}
-                activeClassName="selected"
-                /*activeStyle={{
-                    font: "bold",
-                    background: "#0088cc",
-                    color: "#fff",
-                    border: "0.5em"
-                }}*/
-                className="nav__link"
+            <div className="wrap">
+                <img src={require("../assets/MDS_logo.png")} alt={logo}/>
+            </div>
 
-            >
-                Home
-            </NavLink>
-            <NavLink
-                to={{
-                    pathname: '/about',
+            <div className="wrap">
+                <NavLink
+                    exact to={{
+                    pathname: '/home',
                     state: {prev: false},
                 }}
-                activeClassName="selected"
-                className="nav__link"
-                /*activeStyle={{
-                    font: "bold",
-                    background: "#0088cc",
-                    color: "#fff",
-                    border: "0.5em"
-                }}*/
-            >
-                About
-            </NavLink>
-            <NavLink
-                to={{
-                    pathname: '/products',
-                    state: {prev: false},
-                }}
-                className="nav__link"
-                activeClassName="selected"
-            >
-                Products
-            </NavLink>
-            <NavLink
-                to={{
-                    pathname: '/contact',
-                    state: {prev: false},
-                }}
-                className="nav__link"
-                activeClassName="selected"
-            >
-                Contact
-            </NavLink>
+                    activeClassName="selected"
+                    /*activeStyle={{
+                        font: "bold",
+                        background: "#0088cc",
+                        color: "#fff",
+                        border: "0.5em"
+                    }}*/
+                    className="nav__link"
+
+                >
+                    Home
+                </NavLink>
+                <NavLink
+                    to={{
+                        pathname: '/about',
+                        state: {prev: false},
+                    }}
+                    activeClassName="selected"
+                    className="nav__link"
+                    /*activeStyle={{
+                        font: "bold",
+                        background: "#0088cc",
+                        color: "#fff",
+                        border: "0.5em"
+                    }}*/
+                >
+                    About
+                </NavLink>
+                <NavLink
+                    to={{
+                        pathname: '/products',
+                        state: {prev: false},
+                    }}
+                    className="nav__link"
+                    activeClassName="selected"
+                >
+                    Products
+                </NavLink>
+                <NavLink
+                    to={{
+                        pathname: '/contact',
+                        state: {prev: false},
+                    }}
+                    className="nav__link"
+                    activeClassName="selected"
+                >
+                    Contact
+                </NavLink>
+            </div>
         </nav>
-    );
+);
 }
